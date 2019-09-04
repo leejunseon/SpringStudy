9648ae0bed870761dd4dcf36463a175f9d1ed559
이준선 <ljs921026@gmail.com>
2019-09-04 22:31:17 +0900

    2019-09-04

--- Practice/src/main/java/com/practice/controller/BoardController.java
+++ Practice/src/main/java/com/practice/controller/BoardController.java
@@ -2,17 +2,12 @@ package com.practice.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
